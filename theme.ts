381be58b80dad2dda74bf794be3ae91fdcc7b598
mattponiat <pinkyball50@gmail.com>
2022-01-25 17:41:52 +0100

    Custom font and colors

---
+++ theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const customTheme = extendTheme({
+  fonts: {
+    heading: "nunito",
+    body: "nunito",
+  },
+  colors: {
+    mainBeige: "#FFF7E7",
+    secondaryGreen: "#749B74",
+  },
+});
