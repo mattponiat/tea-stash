1b9d31b6649a0ede96f40af920c6092010aac776
mattponiat <pinkyball50@gmail.com>
2022-01-25 17:54:15 +0100

    Chakra-ui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from "next/app";
+//Fonts
+import "@fontsource/nunito";
+//Custom theme
+import { customTheme } from "theme";
+//Chakra-ui
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+//Components
+import Layout from "components/organisms/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
