cc9705d4ab16532de1f8452c6cfa33d6a0eab038
mattponiat <pinkyball50@gmail.com>
2022-01-25 17:41:14 +0100

    GraphQL setup in Apollo

--- apolloClient.js
+++ apolloClient.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const Client = new ApolloClient({
+const client = new ApolloClient({
   uri: "https://api-eu-central-1.graphcms.com/v2/ckyp0yqas008y01w8gu4rf1k4/master",
   cache: new InMemoryCache(),
 });
 
-export default Client;
+export default client;
