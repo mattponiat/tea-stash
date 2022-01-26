b3544a63f084330e090166297cf4af919cdd4592
mattponiat <pinkyball50@gmail.com>
2022-01-26 14:27:27 +0100

    Logo styling

--- components-ui/atoms/Logo/index.tsx
+++ components-ui/atoms/Logo/index.tsx
@@ -1,21 +1,30 @@
 import * as React from "react";
+import NextLink from "next/link";
 //Chakra-ui
-import { Box, Heading } from "@chakra-ui/react";
+import { Heading, Link } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 
 const Logo = () => {
   return (
-    <Box>
-      <Heading fontWeight="bold" fontSize="32px" w="max-content">
-        <SpanStyled>tea-</SpanStyled>
-        stash
-      </Heading>
-    </Box>
+    <NextLink href="/" passHref>
+      <LinkStyled>
+        <Heading fontWeight="700" fontSize="32px" w="max-content">
+          <SpanStyled>tea</SpanStyled>
+          <span>-stash</span>
+        </Heading>
+      </LinkStyled>
+    </NextLink>
   );
 };
 
 const SpanStyled = styled.span`
   color: green;
+`;
+
+const LinkStyled = styled(Link)`
+  :hover {
+    text-decoration: none;
+  }
 `;
 
 export default Logo;
