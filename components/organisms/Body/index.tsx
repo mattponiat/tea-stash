3a8302156ac7f22d6f178911846758a4c32a755e
mattponiat <pinkyball50@gmail.com>
2022-01-26 19:03:07 +0100

    Body layout

---
+++ components/organisms/Body/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+//Chakra-ui
+import { Box } from "@chakra-ui/react";
+
+const Body = () => {
+  return (
+    <Box as="main">
+      <div></div>
+    </Box>
+  );
+};
+
+export default Body;
