c653f87f3510943960b95cc3d13cec696ca1344b
mattponiat <pinkyball50@gmail.com>
2022-01-29 20:20:26 +0100

    Layout (will change in the future)

--- components/organisms/Layout/index.tsx
+++ components/organisms/Layout/index.tsx
@@ -2,15 +2,16 @@ import * as React from "react";
 //Chakra-ui
 import { Box } from "@chakra-ui/react";
 //Components
-import Header from "components/organisms/Header";
-import Body from "../Body";
+import Header from "components-ui/organisms/Header";
+import Main from "components/organisms/Main";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Header />
-      <Body />
-      <Box>{children}</Box>
+      <Main>
+        <Box>{children}</Box>
+      </Main>
     </>
   );
 };
