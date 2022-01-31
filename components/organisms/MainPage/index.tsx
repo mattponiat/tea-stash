0d0837e1b7336940807c18827fc34fb3c5e3b001
mattponiat <pinkyball50@gmail.com>
2022-01-31 17:23:25 +0100

    New component "MainPage"

---
+++ components/organisms/MainPage/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+//Chakra-ui
+import { Flex, Grid } from "@chakra-ui/react";
+//Components
+import SearchBar from "components/molecules/SearchBar";
+import TeaCard from "components/molecules/TeaCard";
+//Types
+import { ITeaTypes } from "types";
+import { TeasProps } from "types";
+
+const MainPage = ({ teas }: TeasProps) => {
+  return (
+    <Flex flexDirection="column" alignItems="center" marginTop="0.5">
+      <SearchBar teas={teas} />
+      <Grid
+        templateColumns="repeat(auto-fit, minmax(300px, 1fr))"
+        gridGap="1"
+        placeItems="center"
+        border="1px solid black"
+        w="75%"
+      >
+        {teas.map((elem: ITeaTypes, i: number) => {
+          return (
+            <TeaCard
+              key={i + 1}
+              name={elem.name}
+              country={elem.country}
+              type={elem.typeOfTea.name}
+              image={elem.coverImage}
+              slug={elem.slug}
+            />
+          );
+        })}
+      </Grid>
+    </Flex>
+  );
+};
+
+export default MainPage;
