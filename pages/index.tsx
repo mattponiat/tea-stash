eb13cfcdefaf1f4e6b4bcad71ebe9073616fc45a
mattponiat <pinkyball50@gmail.com>
2022-02-01 19:22:59 +0100

    Remove Box component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Head from "next/head";
 //Apollo client
 import { gql } from "@apollo/client";
 import client from "apolloClient";
-//Chakra-ui
-import { Box } from "@chakra-ui/react";
 //Components
 import MainPage from "components/organisms/MainPage";
 //Types
@@ -12,13 +10,14 @@ import { TeasProps } from "types";
 
 const Home: NextPage<TeasProps> = ({ teas }) => {
   return (
-    <Box h="100%" w="100%">
+    <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Teas - tea-stash</title>
       </Head>
+
       <MainPage teas={teas} />
-    </Box>
+    </>
   );
 };
 
