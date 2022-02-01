b08774d8b3ce250cf04104c06330f9b09ab08cba
mattponiat <pinkyball50@gmail.com>
2022-02-01 19:21:56 +0100

    Removed Main component

--- components-ui/organisms/Layout/index.tsx
+++ components-ui/organisms/Layout/index.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 //Chakra-ui
-import { Box } from "@chakra-ui/react";
+// import { Box } from "@chakra-ui/react";
 //Components
 import Header from "components-ui/organisms/Header";
-import Main from "components-ui/organisms/Main";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Header />
-      <Main>{children}</Main>
+      {children}
     </>
   );
 };
