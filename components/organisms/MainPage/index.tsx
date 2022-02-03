59233728357cd22277962104cb13e80c1ddd310b
mattponiat <pinkyball50@gmail.com>
2022-02-03 03:36:58 +0100

    Fix styles Change coverImage to image.url

--- components/organisms/MainPage/index.tsx
+++ components/organisms/MainPage/index.tsx
@@ -2,19 +2,18 @@
 import { Flex, Grid } from "@chakra-ui/react";
 //Components
 import SearchBar from "components/molecules/SearchBar";
-import TeaCard from "components/molecules/TeaCard";
+import TeaCard from "components-ui/molecules/TeaCard";
 //Types
 import { ITeaTypes } from "types";
 import { TeasProps } from "types";
 
 const MainPage = ({ teas }: TeasProps) => {
   return (
-    <Flex flexDirection="column" alignItems="center" marginTop="1" h="auto">
+    <Flex flexDirection="column" alignItems="center" h="auto">
       <SearchBar teas={teas} />
       <Grid
         templateColumns="repeat(auto-fit, minmax(300px, 1fr))"
         placeItems="center"
-        // border="1px solid black"
         w="75%"
         h="auto"
       >
@@ -25,7 +24,7 @@ const MainPage = ({ teas }: TeasProps) => {
               name={elem.name}
               country={elem.country}
               type={elem.typeOfTea.name}
-              image={elem.coverImage}
+              image={elem.image.url}
               slug={elem.slug}
             />
           );
