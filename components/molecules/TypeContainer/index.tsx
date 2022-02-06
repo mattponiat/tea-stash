18162a9dbe0a8d8bced51862c9553582600f1aad
mattponiat <pinkyball50@gmail.com>
2022-02-06 21:26:58 +0100

    Type component for SearchBar

---
+++ components/molecules/TypeContainer/index.tsx
@@ -0,0 +1,62 @@
+//Chakra-ui
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+//Components
+import SearchLabel from "components-ui/atoms/SearchLabel";
+//Types
+import { TeasProps } from "types";
+//Context
+import { useSearchTeasContext } from "context/SearchTeas";
+
+const TypeContainer = ({ teas }: TeasProps) => {
+  const { currentType, setCurrentType } = useSearchTeasContext();
+
+  const uniqueArray = teas.filter(
+    (value, index, array) =>
+      array.findIndex((t) => t.typeOfTea.name === value.typeOfTea.name) ===
+      index
+  );
+
+  return (
+    <Flex w="19rem" mx="auto" h="inherit" alignItems="center">
+      <SearchLabel>Type</SearchLabel>
+      <Menu matchWidth>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon boxSize="1.45rem" />}
+          bg="white"
+          w="100%"
+          h="80%"
+          ml="8"
+          borderRadius="8"
+          boxShadow="0px 0px 1px rgba(0, 0, 0, 0.3)"
+        >
+          {currentType}
+        </MenuButton>
+        <MenuList defaultValue="Any">
+          <MenuItem value="Any" onClick={() => setCurrentType("Any")}>
+            Any
+          </MenuItem>
+          {uniqueArray.map((elem, index) => (
+            <MenuItem
+              key={index + 1}
+              value={elem.typeOfTea.name}
+              onClick={() => setCurrentType(elem.typeOfTea.name)}
+            >
+              {elem.typeOfTea.name}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
+
+export default TypeContainer;
