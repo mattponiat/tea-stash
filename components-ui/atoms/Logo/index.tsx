3c92b432d4be94a00414af57a3fad69c46fbee93
mattponiat <pinkyball50@gmail.com>
2022-02-06 21:30:34 +0100

    Use Chakra-ui's Button

--- components-ui/atoms/Logo/index.tsx
+++ components-ui/atoms/Logo/index.tsx
@@ -7,12 +7,17 @@ const Logo = () => {
   return (
     <LinkBox>
       <Link href="/" passHref>
-        <ButtonStyled>
+        <Button
+          bg="none"
+          p="1"
+          _hover={{ background: "none", color: "secondaryGreen" }}
+          _active={{ background: "none" }}
+        >
           <Heading fontWeight="700" fontSize="2rem" w="max-content">
             <SpanStyled>tea</SpanStyled>
             <span>-stash</span>
           </Heading>
-        </ButtonStyled>
+        </Button>
       </Link>
     </LinkBox>
   );
@@ -20,16 +25,6 @@ const Logo = () => {
 
 const SpanStyled = styled.span`
   color: #749b74;
-`;
-
-const ButtonStyled = styled(Button)`
-  background: none;
-  padding: 0;
-  margin: 0;
-  :hover {
-    background: none;
-    color: #749b74;
-  }
 `;
 
 export default Logo;
