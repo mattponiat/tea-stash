cd90accf1862887382b3721c261312e3036b545d
mattponiat <pinkyball50@gmail.com>
2022-02-09 15:46:20 +0100

    Filter teas function and input Handler

--- context/SearchTeas.tsx
+++ context/SearchTeas.tsx
@@ -1,17 +1,48 @@
 import * as React from "react";
+//Types
+import { Teas } from "types";
 
 const SearchTeas = () => {
   const [currentCountry, setCurrentCountry] = React.useState("Any");
   const [currentType, setCurrentType] = React.useState("Any");
+  const [inputText, setInputText] = React.useState("");
 
   React.useEffect(() => {
     setCurrentCountry(currentCountry);
     setCurrentType(currentType);
-    console.log(currentCountry);
-    console.log(currentType);
   }, [currentCountry, currentType]);
 
-  return { currentCountry, setCurrentCountry, currentType, setCurrentType };
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const lowerCase = e.currentTarget.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  const filterTeas = (teas: Teas) => {
+    let result = teas;
+
+    if (currentType != "Any") {
+      result = result.filter((tea) => tea.typeOfTea.name === currentType);
+    }
+    if (currentCountry != "Any") {
+      result = result.filter((tea) => tea.country === currentCountry);
+    }
+    if (inputText) {
+      result = result.filter((tea) =>
+        tea.name.toLowerCase().includes(inputText)
+      );
+    }
+    return result;
+  };
+
+  return {
+    currentCountry,
+    setCurrentCountry,
+    currentType,
+    setCurrentType,
+    inputText,
+    inputHandler,
+    filterTeas,
+  };
 };
 
 //Context
