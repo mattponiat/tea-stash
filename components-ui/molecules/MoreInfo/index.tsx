bafde72912482ba2d4e0bed3fdd79fef51dfe720
mattponiat <pinkyball50@gmail.com>
2022-02-15 19:55:15 +0100

    MoreInfo component

---
+++ components-ui/molecules/MoreInfo/index.tsx
@@ -0,0 +1,47 @@
+//Chakra-ui
+import { Grid } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+//Types
+import { ITeaTypes } from "types";
+
+const MoreInfo = ({ teas }: { teas: ITeaTypes }) => {
+  return (
+    <Grid
+      templateColumns="repeat(auto-fit, minmax(160px, 1fr))"
+      placeItems="center"
+      h="fit-content"
+      minH="3.7rem"
+      w="80%"
+      mt="44"
+      mb="16"
+      bg="mainBeige"
+      borderRadius="8px"
+      boxShadow="0px 0px 4px rgba(0, 0, 0, 0.3)"
+    >
+      <SpanStyled>
+        Type: <b>{teas.typeOfTea.name}</b>
+      </SpanStyled>
+      <SpanStyled>
+        Country: <b>{teas.country}</b>
+      </SpanStyled>
+      <SpanStyled>
+        City: <b>{teas.city ?? "-"}</b>
+      </SpanStyled>
+      <SpanStyled>
+        Harvest Date: <b>{teas.harvestDate ?? "-"}</b>
+      </SpanStyled>
+      <SpanStyled>
+        Price: <b>{teas.price}$</b>
+      </SpanStyled>
+    </Grid>
+  );
+};
+
+const SpanStyled = styled.span`
+  width: fit-content;
+  height: fit-content;
+  margin: 3px 10px 3px 13px;
+  font-size: 1.125rem;
+`;
+
+export default MoreInfo;
