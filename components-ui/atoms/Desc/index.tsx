1c60c39dbe3801e1455e17d269ecc73384764975
mattponiat <pinkyball50@gmail.com>
2022-02-17 20:43:15 +0100

    Add lineHeight: 1.6

--- components-ui/atoms/Desc/index.tsx
+++ components-ui/atoms/Desc/index.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const Desc = ({ markdown }: { markdown: JSX.Element }) => {
   return (
-    <Box w="95%" minW="250px" fontSize="1.5rem">
+    <Box w="95%" minW="250px" lineHeight="1.6" fontSize="1.5rem" p="1">
       {markdown}
     </Box>
   );
