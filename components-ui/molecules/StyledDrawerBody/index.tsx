66bca7143bc627e0c9077205fe645e6bbb44e5c3
mattponiat <pinkyball50@gmail.com>
2022-03-07 17:43:49 +0100

    Custom Drawer Body

---
+++ components-ui/molecules/StyledDrawerBody/index.tsx
@@ -0,0 +1,27 @@
+//Chakra-ui
+import { DrawerBody } from "@chakra-ui/react";
+//Components
+import StyledDrawerLink from "components-ui/atoms/StyledDrawerLink";
+
+const StyledDrawerBody = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <DrawerBody
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <StyledDrawerLink href="/" onClose={onClose}>
+        Teas
+      </StyledDrawerLink>
+      <StyledDrawerLink href="/vendors" onClose={onClose}>
+        Vendors
+      </StyledDrawerLink>
+      <StyledDrawerLink href="/about" onClose={onClose}>
+        About
+      </StyledDrawerLink>
+    </DrawerBody>
+  );
+};
+
+export default StyledDrawerBody;
