f4508808f1199be771842dec5c753a48042ad027
mattponiat <pinkyball50@gmail.com>
2022-03-08 17:14:28 +0100

    Fix checking if harvestDate exists

--- components-ui/molecules/MoreInfo/index.tsx
+++ components-ui/molecules/MoreInfo/index.tsx
@@ -8,7 +8,7 @@ const MoreInfo = ({ teas }: { teas: ITeaTypes }) => {
     <Flex flexDirection="column" maxW="100%" color="blackAlpha.800">
       {teas.harvestDate ? (
         <Heading as="h3" size="md" mb="2">
-          Harvested in {teas.harvestDate ?? "-"}
+          Harvested in {teas.harvestDate}
         </Heading>
       ) : null}
       <Heading as="h3" size="md">
