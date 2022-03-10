d82f3688758900e73064e60555efa8fd5847ae1d
mattponiat <pinkyball50@gmail.com>
2022-03-10 19:31:17 +0100

    Remove ...props

--- components-ui/atoms/Label/index.tsx
+++ components-ui/atoms/Label/index.tsx
@@ -1,19 +1,9 @@
 //Chakra-ui
 import { Heading } from "@chakra-ui/react";
 
-export const Label = ({
-  children,
-  ...props
-}: {
-  children: React.ReactNode;
-}) => {
+export const Label = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Heading
-      fontSize="1.625rem"
-      color="secondaryGreen"
-      fontWeight="900"
-      {...props}
-    >
+    <Heading fontSize="1.625rem" color="secondaryGreen" fontWeight="900">
       {children}
     </Heading>
   );
