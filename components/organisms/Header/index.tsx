acad183b7f04727abb4d46803c54806680ee96d2
mattponiat <pinkyball50@gmail.com>
2022-03-10 19:30:59 +0100

    Fix Header styles, Rework on Drawer

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -7,28 +7,29 @@ import {
   useDisclosure,
   Drawer,
   DrawerOverlay,
-  DrawerCloseButton,
   DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  Text,
 } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 //Icons
 import { HamburgerIcon } from "@chakra-ui/icons";
+import { TeaIcon } from "icons/TeaIcon";
+import StyledSunIcon from "components-ui/atoms/StyledSunIcon";
 //Components
 import Logo from "components-ui/atoms/Logo";
 import NavBar from "components-ui/molecules/NavBar";
-import StyledSunIcon from "components-ui/atoms/StyledSunIcon";
 import StyledDrawerBody from "components-ui/molecules/StyledDrawerBody";
 //Hooks
 import { useWindowSize } from "usehooks-ts";
-import styled from "@emotion/styled";
 
 const Header = () => {
-  const { width } = useWindowSize();
-  const isSmall = width < 768;
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
-
   const [isLoading, setIsLoading] = React.useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { width } = useWindowSize();
+  const btnRef = React.useRef();
+  const isSmall = width < 768;
 
   React.useEffect(() => {
     setIsLoading(false);
@@ -48,9 +49,9 @@ const Header = () => {
       {isLoading === false && (
         <Grid
           templateColumns={
-            isSmall ? "1fr max-content" : "auto minmax(max-content, 100%) auto"
+            isSmall ? "1fr 1fr" : "auto minmax(max-content, 100%) auto"
           }
-          justifyItems={isSmall ? "space-between" : "center"}
+          justifyItems={isSmall ? "" : "center"}
           alignItems="center"
           maxW="1440px"
           w="95%"
@@ -58,23 +59,25 @@ const Header = () => {
           <Logo />
           {isSmall ? (
             <>
-              <IconButton
-                ref={btnRef.current}
-                onClick={onOpen}
-                aria-label="Navbar"
-                icon={<HamburgerIcon />}
-                color="secondaryGreen"
-                bg="none"
-                border="none"
-                p="0"
-                fontSize="32px"
-                _hover={{
-                  background: "none",
-                }}
-                _active={{
-                  background: "none",
-                }}
-              />
+              <Flex justifyContent="end">
+                <IconButton
+                  ref={btnRef.current}
+                  onClick={onOpen}
+                  aria-label="Navbar"
+                  icon={<HamburgerIcon />}
+                  color="secondaryGreen"
+                  bg="none"
+                  border="none"
+                  p="0"
+                  fontSize="32px"
+                  _hover={{
+                    background: "none",
+                  }}
+                  _active={{
+                    background: "none",
+                  }}
+                />
+              </Flex>
               <Drawer
                 isOpen={isOpen}
                 onClose={onClose}
@@ -83,11 +86,24 @@ const Header = () => {
               >
                 <DrawerOverlay />
                 <StyledDrawerContent
-                  bg="mainBeige"
+                  bg="white"
                   boxShadow="0px -2px 10px inset rgba(0, 0, 0, 0.2)"
                 >
-                  <DrawerCloseButton p="20px" fontSize="20px" />
+                  <DrawerHeader
+                    bg="mainBeige"
+                    boxShadow="0px 0px 1px"
+                    minH="100px"
+                    display="flex"
+                    justifyContent="start"
+                    alignItems="center"
+                  >
+                    <TeaIcon boxSize="5rem" fill="secondaryGreen" />
+                    <Text ml="1" mt="3" fontSize="28px">
+                      Welcome!
+                    </Text>
+                  </DrawerHeader>
                   <StyledDrawerBody onClose={onClose} />
+                  <DrawerFooter></DrawerFooter>
                 </StyledDrawerContent>
               </Drawer>
             </>
