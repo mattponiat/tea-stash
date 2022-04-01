df0dda29ab1604e03030a2ce7a4c5f70fba4cb77
mattponiat <pinkyball50@gmail.com>
2022-04-01 20:05:05 +0200

    Add fade animations and VendorsPage component

--- pages/vendors/index.tsx
+++ pages/vendors/index.tsx
@@ -1,21 +1,22 @@
 import Head from "next/head";
 import { NextPage } from "next";
 //Chakra-ui
-import { Box } from "@chakra-ui/react";
+import { Fade, useDisclosure } from "@chakra-ui/react";
+//Components
+import VendorsPage from "components-ui/organisms/VendorsPage";
 
-const vendors: NextPage = () => {
-  // const { isOpen } = useDisclosure();
+const Vendors: NextPage = () => {
+  const { isOpen } = useDisclosure();
+
   return (
-    // <Fade in={isOpen === false}>
-    <>
+    <Fade in={isOpen === false}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Vendors - tea-stash</title>
       </Head>
-      <Box></Box>
-    </>
-    // </Fade>
+      <VendorsPage />
+    </Fade>
   );
 };
 
-export default vendors;
+export default Vendors;
